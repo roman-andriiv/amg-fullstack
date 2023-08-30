f39a9180b628b0f8c411ce184ed4dfe7f948b501
Roman Andriiv <roman.andriyiv@gmail.com>
2023-08-30 11:49:46 +0200

    [frontend-react] add SideBar component

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -1,8 +1,10 @@
 import {Button} from "@chakra-ui/react";
-
+import SidebarWithHeader from "./shared/SideBar.jsx"
 const App = ()=>{
     return(
-        <Button colorScheme='teal' variant='solid'>Click me</Button>
+        <SidebarWithHeader>
+            <Button colorScheme='teal' variant='solid'>Click me</Button>
+        </SidebarWithHeader>
     )
 }
 
