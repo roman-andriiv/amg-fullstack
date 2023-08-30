8734ab294499034c79bcbbbcc45ddc08d10be0e3
Roman Andriiv <roman.andriyiv@gmail.com>
2023-08-30 11:04:52 +0200

    [frontend-react] install ChakraUI

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -1,58 +1,8 @@
-import {useState} from "react";
-import UserProfile from "./UserProfile.jsx";
+import {Button} from "@chakra-ui/react";
 
-const users = [
-    {
-        name: "Jamila",
-        age: 22,
-        gender: "FEMALE",
-    },
-    {
-        name: "Ana",
-        age: 45,
-        gender: "FEMALE",
-    },
-    {
-        name: "Alex",
-        age: 18,
-        gender: "MALE",
-    },
-    {
-        name: "Bilal",
-        age: 27,
-        gender: "MALE",
-    },
-    {
-        name: "Bob",
-        age: 27,
-        gender: "MALE",
-    },
-]
-
-const UserProfiles = ({users}) => (
-    <div>
-        {users.map((user, index) => (
-            <UserProfile
-                key={index}
-                name={user.name}
-                age={user.age}
-                gender={user.gender}
-                imageNumber={index}
-            />
-        ))}
-    </div>
-)
-
-function App() {
-    const [counter, setCounter] = useState(0)
-    return (
-        <div>
-            <h1>{counter}</h1>
-            <button onClick={() => setCounter(prevCounter => ++prevCounter)}>
-                Increment counter
-            </button>
-            <UserProfiles users={users}/>
-        </div>
+const App = ()=>{
+    return(
+        <Button colorScheme='teal' variant='solid'>Click me</Button>
     )
 }
 
