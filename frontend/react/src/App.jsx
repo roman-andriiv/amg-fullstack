b6b72c5d78d2cbeb12324eb5e31ef9508080d882
Roman Andriiv <roman.andriyiv@gmail.com>
2023-08-31 15:00:25 +0200

    [frontend-react] add AXIOS and getCustomers method, Vite env variable, allow CORS on the backend, render Customers

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -1,9 +1,51 @@
-import {Button} from "@chakra-ui/react";
+import {Spinner, Text} from "@chakra-ui/react";
 import SidebarWithHeader from "./shared/SideBar.jsx"
-const App = ()=>{
-    return(
+import {useEffect, useState} from "react";
+import {getCustomers} from "./services/client.js";
+
+const App = () => {
+
+    const [customers, setCustomers] = useState([])
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        setLoading(true)
+        getCustomers().then(resp => {
+            setCustomers(resp.data)
+        }).catch(err => {
+            console.log(err)
+        }).finally(() => {
+            setLoading(false)
+        })
+    }, [])
+
+    if (loading) {
+        return (
+            <SidebarWithHeader>
+                <Spinner
+                    thickness='4px'
+                    speed='0.65s'
+                    emptyColor='gray.200'
+                    color='blue.500'
+                    size='xl'
+                />
+            </SidebarWithHeader>
+        )
+    }
+
+    if (customers.length <= 0) {
+        return (
+            <SidebarWithHeader>
+                <Text>No customers available</Text>
+            </SidebarWithHeader>
+        )
+    }
+
+    return (
         <SidebarWithHeader>
-            <Button colorScheme='teal' variant='solid'>Click me</Button>
+            {customers.map((customer, index) => (
+                <p key={index}>{customer.name}</p>
+            ))}
         </SidebarWithHeader>
     )
 }
