9ed945da2e816d14d750fa75388187d6247365ea
Roman Andriiv <roman.andriyiv@gmail.com>
2023-09-06 21:20:01 +0200

    [backend-security] send signed Token to Client

--- backend/src/main/java/com/andriiv/amgbackend/customer/CustomerController.java
+++ backend/src/main/java/com/andriiv/amgbackend/customer/CustomerController.java
@@ -1,5 +1,8 @@
 package com.andriiv.amgbackend.customer;
 
+import com.andriiv.amgbackend.jwt.JwtUtil;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,9 +14,11 @@ import java.util.List;
 @RequestMapping("api/v1/customers")
 public class CustomerController {
     private final CustomerService customerService;
+    private final JwtUtil jwtUtil;
 
-    public CustomerController(CustomerService customerService) {
+    public CustomerController(CustomerService customerService, JwtUtil jwtUtil) {
         this.customerService = customerService;
+        this.jwtUtil = jwtUtil;
     }
 
     @GetMapping
@@ -27,8 +32,14 @@ public class CustomerController {
     }
 
     @PostMapping
-    public void registerCustomer(@RequestBody CustomerRegistrationRequest request) {
+    public ResponseEntity<?> registerCustomer(@RequestBody CustomerRegistrationRequest request) {
         customerService.addCustomer(request);
+        String jwtToken = jwtUtil.issueToken(request.email(), "ROLE_USER");
+
+        return ResponseEntity
+                .ok()
+                .header(HttpHeaders.AUTHORIZATION, jwtToken)
+                .build();
     }
 
     @DeleteMapping("{customerId}")
