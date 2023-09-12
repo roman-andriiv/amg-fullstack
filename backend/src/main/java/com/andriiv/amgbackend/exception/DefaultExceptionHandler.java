d8aa0cd1e8a9580c95d0b6cb5b795faeabb533a4
Roman Andriiv <roman.andriyiv@gmail.com>
2023-09-12 16:09:28 +0200

    [backend-security] handle InsufficientAuthenticationException and Exception

--- backend/src/main/java/com/andriiv/amgbackend/exception/DefaultExceptionHandler.java
+++ backend/src/main/java/com/andriiv/amgbackend/exception/DefaultExceptionHandler.java
@@ -3,6 +3,7 @@ package com.andriiv.amgbackend.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -20,5 +21,23 @@ public class DefaultExceptionHandler {
                 new ApiError(request.getRequestURI(), e.getMessage(), HttpStatus.NOT_FOUND.value(), LocalDateTime.now());
 
         return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(InsufficientAuthenticationException.class)
+    public ResponseEntity<ApiError> handleException(InsufficientAuthenticationException e, HttpServletRequest request) {
+
+        ApiError apiError =
+                new ApiError(request.getRequestURI(), e.getMessage(), HttpStatus.FORBIDDEN.value(), LocalDateTime.now());
+
+        return new ResponseEntity<>(apiError, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> handleException(Exception e, HttpServletRequest request) {
+
+        ApiError apiError =
+                new ApiError(request.getRequestURI(), e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value(), LocalDateTime.now());
+
+        return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
