97edb4f9e739bc3da8a20b9d3c72ffc38d807cc0
Roman Andriiv <roman.andriyiv@gmail.com>
2023-09-13 12:56:43 +0200

    [backend-security] Spring Security CORS

--- backend/src/main/java/com/andriiv/amgbackend/security/SecurityFilterChainConfig.java
+++ backend/src/main/java/com/andriiv/amgbackend/security/SecurityFilterChainConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -37,6 +38,7 @@ public class SecurityFilterChainConfig {
 
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/api/v1/customers").permitAll()
                         .anyRequest().authenticated())
