d4c5970b8c223872e5b85d365dfa75aa1178557e
Roman Andriiv <roman.andriyiv@gmail.com>
2023-09-15 10:09:27 +0200

    [frontend-react] add React Router, Login page Component

--- frontend/react/src/components/customer/CustomerCard.jsx
+++ frontend/react/src/components/customer/CustomerCard.jsx
@@ -22,8 +22,8 @@ import {
 } from '@chakra-ui/react'
 import {DeleteIcon} from "@chakra-ui/icons";
 import {useRef} from "react";
-import {deleteCustomer} from "../services/client.js";
-import {errorNotification, successNotification} from "../services/notification.js";
+import {deleteCustomer} from "../../services/client.js";
+import {errorNotification, successNotification} from "../../services/notification.js";
 import UpdateCustomerDrawer from "./UpdateCustomerDrawer.jsx";
 
 export default function CardWithImage({id, name, email, age, gender, imageNumber, fetchCustomers}) {
