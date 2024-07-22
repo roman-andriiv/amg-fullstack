0d476dd9d5c495062ce057b3f3bc4a07da264673
Roman Andriiv <roman.andriiv.dev@gmail.com>
2024-07-22 22:01:06 +0200

    [backend]: Update dependencies

--- backend/src/main/java/com/andriiv/amgbackend/customer/CustomerDto.java
+++ backend/src/main/java/com/andriiv/amgbackend/customer/CustomerDto.java
@@ -6,7 +6,13 @@ import java.util.List;
  * Created by Roman Andriiv (11.09.2023 - 15:05)
  */
 
-public record CustomerDto(Integer id, String name, String email, Gender gender, Integer age, List<String> roles,
-                          String username) {
-    
+public record CustomerDto(
+        Integer id,
+        String name,
+        String email,
+        Gender gender,
+        Integer age,
+        List<String> roles,
+        String username) {
+
 }
