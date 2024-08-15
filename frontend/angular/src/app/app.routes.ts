7fd8991064b0cc57dcbf30052029484f9d34a620
Roman Andriiv <roman.andriiv.dev@gmail.com>
2024-08-15 22:27:35 +0200

    [frontend-angular]: create the Customer component

--- frontend/angular/src/app/app.routes.ts
+++ frontend/angular/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import {Routes} from '@angular/router';
+import {CustomerComponent} from "./components/customer/customer.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+  path: 'customer',
+  component: CustomerComponent
+}];
